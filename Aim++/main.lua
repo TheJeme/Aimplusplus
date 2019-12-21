fd9304daf731824ad40ec45c96554c9d04859c0e
Jeme <43149988+TheJeme@users.noreply.github.com>
2019-12-21 11:27:21 +0200

    Add files via upload

--- Aim++/main.lua
+++ Aim++/main.lua
@@ -6,28 +6,36 @@ local gh
 local points
 local mx, my
 local lives
-local difficult
-local targetSize
+local targets
 local time
 local misses
+local misshits
 local alive
-local angle
+local clicks
+local playedYet
+local elapsedTime
+local elapsedTimer
 
 function love.load()
   gw = love.graphics.getWidth()
   gh = love.graphics.getHeight()
   bigFont = love.graphics.newFont("roboto.ttf", 124)
-  font = love.graphics.newFont("roboto.ttf", 54)
+  font = love.graphics.newFont("roboto.ttf", 52)
   littleFont = love.graphics.newFont("roboto.ttf", 18)
+  arimoFont = love.graphics.newFont("Arimo-Bold.ttf", 13)
   hitSound = love.audio.newSource("Hit.wav", "static")
   missSound = love.audio.newSource("Miss.wav", "static")
   points = 0
   misses = 0
+  misshits = 0
+  clicks = 0
   lives = 3
-  difficult = 1
-  targetSize = 3
+  targets = 0
   time = 30
+  elapsedTime = 0
+  elapsedTimer = 0
   alive = false
+  playedYet = false
 
   listOfCircles = { }
   listofCrosses = { }
@@ -36,7 +44,6 @@ function love.load()
 
 end
 
-local elapsedTime = 0
 function love.update(dt)
   gooi.update(dt)
   mx, my = love.mouse.getPosition()
@@ -52,17 +59,28 @@ function love.update(dt)
       v.radius = v.radius - v.speed * dt
     else
       v.reverse = true
+        end
       end
     end
   end
 end
   elapsedTime = elapsedTime + dt
 
-  if(elapsedTime > 0.4) then
+  if(elapsedTime >= 2 / spinnerSpawnSpeed.value and alive) then
       createCircle()
+      targets = targets + 1
       elapsedTime = 0
-    end
   end
+
+  elapsedTimer = elapsedTimer + dt
+
+    if(elapsedTimer >= 1.0) then
+      elapsedTimer = 0
+      time = time - 1
+      if(time == 0) then
+        die()
+      end
+    end
 end
 
 function love.draw()
@@ -75,8 +93,19 @@ function love.draw()
     love.graphics.setFont(bigFont)
     love.graphics.print("Aim++", gw / 2 - 180, 140)
     love.graphics.setColor(0.3, 0.3, 0.3, 1)
-    love.graphics.rectangle('fill', gw / 2 + 225, gh / 2 - 175, 250, 300)
-    love.graphics.rectangle('fill', gw / 2 - 475, gh / 2 - 175, 250, 300)
+    love.graphics.rectangle('fill', gw / 2 + 225, gh / 2 - 175, 250, 325)
+    if (playedYet) then
+      love.graphics.rectangle('fill', gw / 2 - 475, gh / 2 - 175, 250, 325)
+      love.graphics.setColor(1, 1, 1, 1)
+      love.graphics.setFont(arimoFont)
+      love.graphics.printf("Stats", gw / 2 - 460, gh / 2 - 150, 222, 'center')
+      love.graphics.printf(points .." Hits", gw / 2 - 460, gh / 2 - 100, 222, 'center')
+      love.graphics.printf(clicks .." Clicks", gw / 2 - 460, gh / 2 - 60, 222, 'center')
+      love.graphics.printf(misshits .." Missed Hits", gw / 2 - 460, gh / 2 - 20, 222, 'center')
+      love.graphics.printf(misses .." Missed Targets", gw / 2 - 460, gh / 2 + 20, 222, 'center')
+      love.graphics.printf(string.format("%0.2f", points / clicks) .."% Hit Accuracy", gw / 2 - 460, gh / 2 + 60, 222, 'center')
+      love.graphics.printf(string.format("%0.2f", points / targets) .."% Target Efficiency", gw / 2 - 460, gh / 2 + 100, 222, 'center')
+    end
     gooi.draw()
   end
 
@@ -102,14 +131,23 @@ function love.keypressed(key)
       if alive == true then
         die()
       else
-        love.event.quit()
+        gooi.confirm({
+          text = "Are you sure you want to exit?",
+          ok = function()
+            love.event.quit()
+          end})
       end
     end
 end
 
 
 function love.mousepressed(x, y,button)
-  gooi.pressed()
+  if not alive then
+    gooi.pressed()
+  else
+    playedYet = true
+    clicks = clicks + 1
+  end
   hit = false
   for i, v in ipairs(listOfCircles) do
    local isMouseOnCircle = mx > v.x - v.radius and mx < v.x + v.radius and
@@ -126,6 +164,7 @@ function love.mousepressed(x, y,button)
    end
    if hit == false and alive then
     missSound:play()
+    misshits = misshits + 1
     createCross()
     lives = lives - 1
     if lives == 0 then
@@ -159,7 +198,7 @@ function createUI()
     gooi.newButton({text = "Exit", x = gw / 2 - 120, y = gh / 2 + 50, w = 240, h = 70})
       :onRelease(function()
         gooi.confirm({
-          text = "Are you sure?",
+          text = "Are you sure you want to exit?",
           ok = function()
             love.event.quit()
           end
@@ -167,30 +206,24 @@ function createUI()
       end)
 
       gooi.newLabel({x = gw / 2 + 250, y = gh / 2 - 100, text = "Difficulty"})
-      spinnerDiff = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 - 100, min = 1, max = 3, value = difficult})
+      spinnerDiff = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 - 100, min = 1, max = 10, value = 3})
       gooi.newLabel({x = gw / 2 + 250, y = gh / 2 - 50, text = "Time"})
-      spinnerTime = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 - 50, min = 0, max = 120, value = time})
+      spinnerTime = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 - 50, min = 0, max = 240, value = 30})
       gooi.newLabel({x = gw / 2 + 250, y = gh / 2, text = "Target Size"})
-      spinnerLifes = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2, min = 0, max = 10, value = targetSize})
+      spinnerTargetSize = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2, min = 1, max = 10, value = 3})
       gooi.newLabel({x = gw / 2 + 250, y = gh / 2 + 50, text = "Lives"})
-      spinnerLifes = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 + 50, min = 0, max = 99, value = lifes})
-
-
-      gooi.newLabel({x = gw / 2 - 350, y = gh / 2 - 160, text = "Stats"})
-
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2 - 120, text = "0 Points"})
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2 - 80, text = "290 Hits"})
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2 - 40, text = "0 Misses"})
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2, text = "75% Click Accuracy"})
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2 + 40, text = "0% Target Efficiency"})
-      gooi.newLabel({x = gw / 2 - 370, y = gh / 2 + 80, text = "0% Target Per Second"})
-
-
-
+      spinnerLifes = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 + 50, min = 0, max = 99, value = 5})
+      gooi.newLabel({x = gw / 2 + 250, y = gh / 2 + 100, text = "Spawn Speed"})
+      spinnerSpawnSpeed = gooi.newSpinner({x = gw / 2 + 325, y = gh / 2 + 100, min = 0, max = 99, value = 4})
 end
 
 function startGame()
  alive = true
+ points = 0
+ clicks = 0
+ misses = 0
+ misshits = 0
+ targets = 0
  time = spinnerTime.value
  lives = spinnerLifes.value
 end
@@ -208,18 +241,18 @@ function createArea()
     love.graphics.setColor(1, 0.5, 0, 1)
     love.graphics.setFont(font)
     love.graphics.print('Time: '.. time, 600, 0)
-    love.graphics.print('Points: '.. points, 900, 0)
+    love.graphics.print('Hits: '.. points, 900, 0)
     love.graphics.print('Lifes: '.. lives, 1200, 0)
   love.graphics.pop()
 end
 
 function createCircle()
   circle = {}
-  circle.x = love.math.random(80, gw - 80)
-  circle.y = love.math.random(60, gh - 60)
-  circle.radius = 4
-  circle.maxRadius = 30
-  circle.speed = 17
+  circle.x = love.math.random(90, gw - 90)
+  circle.y = love.math.random(70, gh - 70)
+  circle.radius = 1
+  circle.maxRadius = 10 * spinnerTargetSize.value
+  circle.speed = 5 * spinnerDiff.value
   circle.reverse = false
 
   table.insert(listOfCircles, circle)
